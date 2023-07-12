26113137d5db45f01377aceb5b9874344abb7ebb
miguel-uribe <miguel-uribe@users.noreply.github.com>
2023-07-12 08:33:58 -0500

    updated the layout interpreter and created the traffic_lights_file

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Future modifications:
 
 - Each line must contain the information of the breaks. This information must be used to determine the distance to the next stop and to apply the boundary conditions at each step.
 - To avoid unwanted lane changes, only changes to a lane where the end of lane is larger will be allowed.
+- The services that change also have a service end, by default. Therefore, at service ends it should be checked whether a service change takes place
+- At intersections, a rule should be created to avoid buses to interfere in the intersections.
--- layout/layout_interpreter.py
+++ layout/layout_interpreter.py
@@ -12,7 +12,36 @@ import pandas as pd
 import numpy as np
 from parameters import *
 import os
+# we locate the current file position
+wd = os.path.dirname(__file__)
 
+#################################################################
+############# now we start building the traffic lights information
+traffic_lights_df = pd.read_excel(os.path.join(wd,'traffic_lights_file.xlsx'))
+traffic_lights = {}
+for index, row in traffic_lights_df.iterrows():
+    tlname = row['name']
+    traffic_lights[tlname] = {}
+    traffic_lights[tlname]['dir'] = [info.strip() for info in row['direction'].split('-')] # we read all the directions
+    traffic_lights[tlname]['lengths'] = [int(info.strip()) for info in row['length'].split('-')] # we read all the lengths
+    if (len(traffic_lights[tlname]['dir'])!=len(traffic_lights[tlname]['lengths'])): # we check that the number of directions and lengths is the same
+        print(f"Warning!! Traffic light {tlname} has a different number of directions than lengths")
+    phase = 0
+    traffic_lights[tlname]['phases'] = []   # we create a list of phases
+    while(type(row[f'phase_{phase}'])==str):   # we only scan until a phase is not a string
+        duration, scheme = row[f'phase_{phase}'].split(':')  # we get the information 
+        duration = int(duration) # the duration must be an integer
+        scheme = [int(info.strip()) for info in scheme.split('-')] # the scheme is an integer list of 0's and 1's
+        phase += 1
+        traffic_lights[tlname]['phases'].append((duration, scheme))
+        if (len(traffic_lights[tlname]['dir'])!=len(scheme)):
+            print(f"Warning!!! Traffic light {tlname} has a scheme with a different number of elements than the number of possible directions")
+        
+    traffic_lights[tlname]['pos'] = [None]*len(traffic_lights[tlname]['dir'])
+
+
+###################################################################
+############## we load the layout file information
 # we locate the current file position
 wd = os.path.dirname(__file__)
 # We initially import the layout file
@@ -25,7 +54,7 @@ length = len(layout_df)
 lanes = {} # the arrays with the available lanes for each service
 stationdefinition = {} # the list of doors in each station
 servicedefinition = {} # The list of stops for each service
-traffic_lights = {} # The sucession of traffic lights per route
+servicetrafficlights = {} #The succession of traffic lights per route
 servicebreaks = {} # the list of breaks in the system, for each service
 servicestarts = {} # the position where each service starts
 serviceends = {} # the position where each service ends
@@ -75,24 +104,33 @@ for index, row in layout_df.iterrows():
                     if len(stopdata)==1:
                         # we add the stop to all services
                         for service in services:
-                            servicedefinition[service].extend([name,pos])
+                            servicedefinition[service].append((name,pos))
                     # if there are more than 1 element, only some services make their stop
                     else:
                         new_services = [data.strip() for data in stopdata[1:]]
                         # we add the stop to the specific services
                         for service in new_services:
-                            servicedefinition[service].extend([name,pos])
+                            servicedefinition[service].append((name,pos))
                             
                 ################################################
                 #S, the traffic light information
                 elif itemclass == 'S':
+
+                    # we extract the name and direction
+                    tlname, tldir = [info.strip() for info in iteminfo.split(',')]
+                    # we add the position information to the traffic_lights configuration
+                    tlindex = np.where(np.array(traffic_lights[tlname]['dir'])==tldir)[0][0]
+                    traffic_lights[tlname]['pos'][tlindex] = index
                     # we loop over the services
                     for service in services:
                         # we check  whether the service is already in the traffic lights
-                        if service in traffic_lights.keys():
-                            traffic_lights[service].extend([info.strip() for info in iteminfo.split(',')])
+                        if service in servicetrafficlights.keys():
+                            servicetrafficlights[service].append((tlname, tldir))
                         else:
-                            traffic_lights[service] = [info.strip() for info in iteminfo.split(',')]
+                            servicetrafficlights[service] = [(tlname, tldir)]
+                        # we also add the information of the traffic light
+                        
+                        
                 ################################################
                 #B, the bridge information
                 elif itemclass == 'B':
@@ -134,17 +172,132 @@ for index, row in layout_df.iterrows():
                 elif itemclass == 'C':
                     # we get the change information
                     org_service, final_service = [data.strip() for data in iteminfo.split(',')]
-                    # we check  whether the service is alredy in the changes list
+                    # we check  whether the service is already in the changes list
                     if org_service in servicechanges.keys():
                         servicechanges[org_service].append([final_service, index])
                     else:
                         servicechanges[service] = [[final_service, index]]
+                    # we check  whether the service is already in the finish dictionary
+                    if org_service in serviceends.keys():
+                        print(f"Error. Service {org_service} has been introduced more than once in serviceends at position {index}")
+                    else:
+                        serviceends[org_service] = index
         except:
             pass
-        
+
+# for each service, the servicedefinition, the traffic_lights information, and the breaks information must be sorted based on the service start position
+# sorting the breaks first:
+for key in servicebreaks.keys():
+    ord_index = []
+    # only if there are more than one breaks it becomes important to sort
+    if len(servicebreaks[key])>1:
+        breaks = np.array(servicebreaks[key])
+        # we now find the first break after the service starts
+        ord_index.append(np.where(breaks[:,0] > servicestarts[key])[0][0])
+        # we now start finding the first break after the last break
+        while (len(ord_index)<len(breaks)):
+            # the last break position
+            lastbreak = breaks[ord_index[-1], 2]
+            # we find the next break after the last break
+            ord_index.append(np.where(breaks[:,0] > lastbreak)[0][0])
+        # the service breaks are reordered using ord_index
+        servicebreaks[key][:] = [servicebreaks[key][i] for i in ord_index]
+
+# we now proceed to sort the stations
+for key in servicedefinition.keys():
+    ord_index = [] # the list with the organized index
+    track_index = [] # a list with the section number 
+    breaksexists = False # By default there are no breaks
+    # we create an array of stops
+    stops = np.array([stationdefinition[servicedefinition[key][i][0]][servicedefinition[key][i][1]] for i in range(len(servicedefinition[key]))] )
+    # we load the array of breaks
+    if key in servicebreaks.keys():
+        breaks = np.array(servicebreaks[key])
+        breaksexists = True
+    # we build the sections
+    if not breaksexists:
+        sections = [(servicestarts[key], serviceends[key])]
+    else:
+        sections = [(servicestarts[key], breaks[0,0])]
+        for i in range(len(breaks)-1):
+            sections.append((breaks[i,2], breaks[i+1,0]))
+        sections.append((breaks[-1,2], serviceends[key]))
+    # We look for the first stop in a section
+    for i,section in enumerate(sections):
+        firststop = np.where((stops >= section[0]) & (stops<= section[1]) )[0]
+        if len(firststop)>0:
+            next_index = firststop[0]
+            track = i
+            break
+    ord_index.append(next_index)
+    track_index.append(track)
+    # we repeat this process until all stations are covered
+    while (len(ord_index) < len(servicedefinition[key])):
+        next_index += 1
+        if breaksexists:
+            if stops[next_index] > breaks[track][0]:
+                next_index = np.where(stops > breaks[track][2])[0][0]
+                track += 1
+                if (track == len(breaks)):
+                    breaksexists = False
+        ord_index.append(next_index)
+        track_index.append(track)
+    if (len(ord_index)!= len(set(ord_index))):
+        print(f"Warning!!! The indices in ord_index for service {key} when ordering the stops are not unique")
+    # we reorder the stop list
+    servicedefinition[key][:] = [servicedefinition[key][i] for i in ord_index]
+
+
+# now we proced to sort the traffic_lights
+for key in servicetrafficlights.keys():
+    ord_index = [] # the list with the organized index
+    track_index = [] # a list with the section number 
+    breaksexists = False # By default there are no breaks
+    # we create an array with the position of all traffic lights
+    tlights = np.array([traffic_lights[name]['pos'][np.where(np.array(traffic_lights[name]['dir'])==dir)[0][0]] for name, dir in servicetrafficlights[key]] )
+    # we load the array of breaks
+    if key in servicebreaks.keys():
+        breaks = np.array(servicebreaks[key])
+        breaksexists = True
+    # we build the sections
+    if not breaksexists:
+        sections = [(servicestarts[key], serviceends[key])]
+    else:
+        sections = [(servicestarts[key], breaks[0,0])]
+        for i in range(len(breaks)-1):
+            sections.append((breaks[i,2], breaks[i+1,0]))
+        sections.append((breaks[-1,2], serviceends[key]))
+    #print(sections)
+    # We look for the first traffic light in a section
+    for i,section in enumerate(sections):
+        firstlight = np.where((tlights >= section[0]) & (tlights<= section[1]) )[0]
+        if len(firstlight)>0:
+            next_index = firstlight[0]
+            track = i
+            break
+    ord_index.append(next_index)
+    track_index.append(track)
+    # we repeat this process until all traffic lights are covered
+    while (len(ord_index) < len(servicetrafficlights[key])):
+        next_index += 1
+        if breaksexists:
+            if tlights[next_index] > breaks[track][0]:
+                next_index = np.where(tlights > breaks[track][2])[0][0]
+                track += 1
+                if (track == len(breaks)):
+                    breaksexists = False
+        ord_index.append(next_index)
+        track_index.append(track)
+    #print(key, ord_index, track_index)
+    if (len(ord_index)!= len(set(ord_index))):
+        print(f"Warning!!! The indices in ord_index for service {key} when ordering the stops are not unique")
+    # we reorder the stop list
+    servicetrafficlights[key][:] = [servicetrafficlights[key][i] for i in ord_index]
+
 print(stationdefinition)
 print(servicedefinition)
 print(traffic_lights)
+print(servicetrafficlights)
 print(servicebreaks)
 print(servicestarts)
 print(serviceends)
@@ -210,6 +363,8 @@ name = os.path.join(os.pardir,'conf','station_list.txt')
 np.savetxt(name,  np.hstack( ( np.arange(len(stations), dtype = int).reshape(-1,1), stations.reshape(-1,1) ) ) , fmt = '%s %s')
 
 # we now create the station definition file
+# the format of the file is as follows
+# station index - [position, biarticulated capability] (over all docking bays)
 name = os.path.join(os.pardir,'conf','station_definition.txt')
 text = ''
 for i, station in enumerate(stations):
@@ -217,9 +372,10 @@ for i, station in enumerate(stations):
     for dock in stationdefinition[station]:
         text = text + f' {dock} 0' # by default, no biarticulated buses are allowed at any docking bay
     text = text + '\n'
-deffile = open(name, 'w')
-deffile.write(text)
-deffile.close()
+# we export the file
+with open(name, 'w') as deffile:
+    deffile.write(text)
+
 
 #################################################################
 ############# now we start building the service information
@@ -228,3 +384,39 @@ services = np.array(list(servicedefinition.keys()))
 # we export the service list
 name = os.path.join(os.pardir,'conf','service_list.txt')
 np.savetxt(name,  np.hstack( ( np.arange(len(services), dtype = int).reshape(-1,1), services.reshape(-1,1) ) ) , fmt = '%s %s')
+
+# for each service we create the service definition file as a series with the station and docking bay information
+# the format of the file is as follows
+# service index - service start - service end - [station index, docking bay index] (over all stops of the service)
+name = os.path.join(os.pardir,'conf','service_definition.txt')
+text = ''
+for i, service in enumerate(services):
+    text = text + f'{i} {servicestarts[service]} {serviceends[service]}'
+    for station, dock in servicedefinition[service]:
+        index = np.where(stations == station)[0][0]
+        text = text + f' {index} {dock}'
+    text = text + '\n'
+# we export the file
+with open(name, 'w') as deffile:
+    deffile.write(text)
+
+# we now create a file specifying the service breaks
+# the format of the file is as follows
+# service index - [origin pos, origin lane, destination pos, destination lane] (over all the breaks)
+name = os.path.join(os.pardir,'conf','service_breaks.txt')
+text = ''
+for i, service in enumerate(services):
+    text = text + f'{i}'
+    if service in servicebreaks.keys():
+        for or_pos, or_lane, des_pos, des_lane in servicebreaks[service]:
+            text = text + f' {or_pos} {or_lane} {des_pos} {des_lane}'
+        text = text + '\n'
+    else:
+        text = text + '\n'
+# we export the file
+with open(name, 'w') as breakfile:
+    breakfile.write(text)
+    
+# we now create a file specifying the traffic lights for each service
+# the format of the file is as follows
+# service index - [traffic light index, direction] (over all the traffic lights)
