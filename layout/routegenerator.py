bd912ea6efd2c3241b101f4a6cb030653aded88a
miguel-uribe <miguel-uribe@users.noreply.github.com>
2023-08-02 10:28:22 -0500

    Finished the route generator

--- layout/routegenerator.py
+++ layout/routegenerator.py
@@ -22,97 +22,89 @@ stations = []
 for stlist in service_stops:
     stations = stations + stlist
 stations = list(set(stations))
-print(stations)
+
 
 # Defining the route matrix with empty list elements
 RouteMatrix=[[[] for x in range(len(stations))] for x in range(len(stations))]
 
 # The initial station ID
-initstationID=0
+initstationID = 0
 
 #The origins element (updated on everystep)
 origins=[]
 #In the first step, the origins is simply the initial station
 origins.append(initstationID)
 
-#Creating the seed route for all strations
-for station in stations:
-    RouteMatrix[station][station].append([station,station,0])
-
-for i in range(2*len(stations)):
+# Looking for all possible direct routes between stations
+# No bus changes are included in the list
+for epoc in range(10):
     #Allocating memory for the destination, line and origin list
-    destination=[]
-    lines=[]
-    origins=[]
+    destinations=[]
     # First we find the possible destinations from the origins
     for station in origins:
-        # we scan all the sevices
-        for line in service_stops:
+        # we scan all the services
+        for i,line in enumerate(service_stops):
             # we find the index of the stop
             try:
                 stindex = line.index(station)
             except:
-                stindex = 45
-        # Adding the new information
-        destinationIDs=destinationIDs+destinationaux
-        lineIDs=lineIDs+lineaux
-        originIDs=originIDs+originaux
-print(RouteMatrix)
+                stindex = np.nan
+            # in case the origin is in the service
+            if ~np.isnan(stindex):
+                # we scan over the following stops
+                for j in range(stindex+1,len(line)):
+                    destinations.append(line[j])
+                    # we define the itinerary as service - number of stops
+                    itinerary = [i, j-stindex]
+                    # we check whether the itinerary already exists
+                    if itinerary not in RouteMatrix[station][line[j]]:
+                        RouteMatrix[station][line[j]].append(itinerary)
+                        #print('Added new itinerary', epoc)
     
-'''
-Copiado del original
+    origins = list(set(destinations))
+
+# calculating the weigth of each itinerary
+RouteWeight = [[[] for i in range(len(stations))] for j in range(len(stations))]
+# Normalizing the weights
+for i in range(len(stations)):
+    for j in range(len(stations)):
+        for k in range(len(RouteMatrix[i][j])):
+            factor=1
+            RouteWeight[i][j].append(np.exp(-factor*np.array(RouteMatrix[i][j][k][1]))/np.sum(np.exp(-factor*np.array(RouteMatrix[i][j])[:,1])))
 
 
+# Printing the route matrix 
+filename=os.path.join(wd,os.pardir,'conf', "RouteMatrix.txt")
+routefile=open(filename,'w')
 
-# The algorithm stops when all stations are covered
-#for station in Stations:
-for i in range(2*len(Stations)):
-    #Allocating memory for the destination, line and origin list
-    destinationIDs=[]
-    lineIDs=[]
-    originIDs=[]
-    # First we find the possible destinations from the origins
-    for stationID in origins:
-        [destinationaux,lineaux,originaux]=lineC.getdestinations(Stations,stationID,Lines)
-        # Adding the new information
-        destinationIDs=destinationIDs+destinationaux
-        lineIDs=lineIDs+lineaux
-        originIDs=originIDs+originaux
+for i in range(len(stations)):
+    for j in range(len(stations)):
+        routefile.write("%d %d\n"%(i,j))
+        routefile.write("%d\n"%len(RouteMatrix[i][j]))
+        for k,route in enumerate(RouteMatrix[i][j]):
+            routefile.write("%f\n"%RouteWeight[i][j][k])
+            routefile.write("%d %d\n"%(RouteMatrix[i][j][k][0],RouteMatrix[i][j][k][1]))                
+routefile.close()
+
+###############
+## OD Matrix
+
+# Printing the OD matrix
+filename=os.path.join(wd,os.pardir,'conf', "OD_matrix.txt")
+odfile=open(filename,'w')
+
+for i in range(len(stations)):
+    destinations = np.random.random(len(stations))
+    # making the diagonal 0
+    destinations[i] = 0
+    # correcting for non connected stations
+    for j in range(len(stations)):
+        if len(RouteMatrix[i][j])==0:
+            destinations[j] = 0
+    # normalizing
+    destinations = destinations/np.sum(destinations)
+    # correcting for non connected stations
+    for j in range(len(stations)):
+        odfile.write(f'{i} {j} {destinations[j]}\n')
         
-    # Creating a warning in case the size of the three vectors is different
-    if len(destinationIDs)!=len(lineIDs) or len(destinationIDs)!=len(originIDs) or len(lineIDs)!=len(originIDs):
-        print("Warning! The destinationIDs, lineIDs and originIDs vectors have different size")
-        break
-            
-    # Creating the routes
-    for destination,line,origin in zip(destinationIDs,lineIDs,originIDs):
-        # Sweeping over all stations
-        for station in Stations:
-            # Checking all routes from station to origin
-            for route in RouteMatrix[station.ID][origin]:
-                # Copying the route to reach the origin
-                routeaux=copy.deepcopy(route)
-                # Only if the destination is not already in the route 
-                if routeC.changenotincluded(routeaux,destination):
-                    # Adding the new routefragment
-                    routeaux.addstop(line,destination)
-                    # only if the number of fragments is not larger than 3
-                    if len(routeaux.fragments)<4:
-                        # Checking the new route is not already there
-                        alreadythere=False
-                        for route1 in RouteMatrix[station.ID][destination]:
-                            alreadythere=alreadythere+routeC.compareroutes(route1,routeaux)
-                        if alreadythere==0:
-                            # Adding the newly created route to the matrix
-                            routeC.insertRoute(RouteMatrix[station.ID][destination],routeaux)    
-    #    print(destinationIDs)
-    
-    # Removing duplicates
-    destinationIDs=list(set(destinationIDs))
-#    print(destinationIDs)
-
-    # updating the origins
-    origins=list(destinationIDs)
-    print(origins)
-'''
-print(service_stops)
+odfile.close()
